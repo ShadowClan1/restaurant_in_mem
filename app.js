845fee7551d3eb5b64b6535a5371af3c5023f038
Ritik0K <ritik.k.dev@gmail.com>
2024-04-22 21:17:41 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+app.use(require('cors')())
+
+
+
+let restauratants =[
+  { id : 1,
+    name :"Deepak Restaurant",
+    description : "This is the dynamic description",
+    avg_rating :3.5 
+},
+  { id :2,
+    name :"Rahul Restaurant",
+    description : "This is the dynamic description",
+    avg_rating :4 
+},
+  { id :3,
+    name :"Rohit Restaurant",
+    description : "This is the dynamic description",
+    avg_rating :2 
+},
+  { id :4,
+    name :"Nishant Restaurant",
+    description : "This is the dynamic description",
+    avg_rating :5 
+}
+]
+
+app.post('/add-restaurant',(req,res)=>{
+    let {name, description, image, rating} = req.body;
+
+    restauratants.push({name, avg_rating , description, image, id :restauratants.length+1 })
+    return res.status(200).json({restauratants})
+})
+
+app.get('/list', (req, res)=>{
+    return res.status(200).json({restauratants})
+})
+
+
+app.post('/add-rating',(req,res)=>{
+    let {id, rating ,name , description} = req.body;
+  let curRes =   restauratants.find((e)=>e.id === id)
+  if(!curRes){
+    return res.status(400).json({error : "Restaurant not found with this id", success :false})
+  }
+    curRes.avg_rating = rating
+  return res.status(200).json({data : [...restauratants], success :true})
+})
+
+app.listen(5000, ()=>{
+    console.log("Server is listening on port 5000")
+})
