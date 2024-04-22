3b05e5d9c4e48973609978d385d11921fbc8b6d6
Ritik0K <ritik.k.dev@gmail.com>
2024-04-22 21:54:31 +0530

    rating changes

--- app.js
+++ app.js
@@ -10,29 +10,33 @@ let restauratants =[
   { id : 1,
     name :"Deepak Restaurant",
     description : "This is the dynamic description",
-    avg_rating :3.5 
+    rating :3324 ,
+    total_ratings :720
 },
   { id :2,
     name :"Rahul Restaurant",
     description : "This is the dynamic description",
-    avg_rating :4 
+    rating :2324 ,
+    total_ratings :680
 },
   { id :3,
     name :"Rohit Restaurant",
     description : "This is the dynamic description",
-    avg_rating :2 
+    rating :332 ,
+    total_ratings :100
 },
   { id :4,
     name :"Nishant Restaurant",
     description : "This is the dynamic description",
-    avg_rating :5 
+    rating :5 ,
+    total_ratings :1
 }
 ]
 
 app.post('/add-restaurant',(req,res)=>{
     let {name, description, image, avg_rating} = req.body;
 
-    restauratants.push({name, avg_rating , description, image, id :restauratants.length+1 })
+    restauratants.push({name, avg_rating , description, total_rating :0, rating :0, id :restauratants.length+1 })
     return res.status(200).json({data :[...restauratants], success :true})
 })
 
@@ -44,19 +48,23 @@ app.get('/list', (req, res)=>{
 app.post('/add-rating',(req,res)=>{
     let {id, rating} = req.body;
   let curRes =   restauratants.find((e)=>e.id === id)
+  
   if(!curRes){
     return res.status(400).json({error : "Restaurant not found with this id", success :false})
   }
-    curRes.avg_rating = rating
+  if(rating > 5 ||rating <0 || Number.isNaN(rating)){
+    return res.status(400).json({error : "Rating should be number between 0-5", success :false})
+  }
+    curRes.rating = rating
+    curRes.total_ratings++;
   return res.status(200).json({data : [...restauratants], success :true})
 })
 app.post('/update-restaurant',(req,res)=>{
-    let {id, avg_rating, name, description} = req.body;
+    let {id, rating, name, description} = req.body;
   let curRes =   restauratants.find((e)=>e.id === id)
   if(!curRes){
     return res.status(400).json({error : "Restaurant not found with this id", success :false})
   }
-    curRes.avg_rating = avg_rating
     curRes.name = name
     curRes.description = description
   return res.status(200).json({data : [...restauratants], success :true})
